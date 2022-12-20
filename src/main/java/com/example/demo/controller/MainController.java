f89bf0c270577fd31ae54643f1d10617ea5e7c4e
DESKTOP-TAT098A\Admin <lamtu199513@gmail.com>
2022-12-20 18:31:37 +0700

    add key value in properties file

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Value("${my.key}")
+    private String hello;
+
+    @Autowired
+    private Environment evn;
+
     @GetMapping("/")
     public String home(Model model) {
+        System.out.println(hello);
+        System.out.println(evn.getProperty("my.key"));
         model.addAttribute("msg", "Hello Day La Lam");
         return "index";
     }
