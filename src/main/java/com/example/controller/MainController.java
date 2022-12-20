a6a4b3d08f8956cc0c34701ac353c2d7195c1786
DESKTOP-TAT098A\Admin <lamtu199513@gmail.com>
2022-12-20 19:32:20 +0700

    Spring message in Spring boot

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -1,8 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @Value("${my.key}")
-    private String hello;
-
     @Autowired
-    private Environment evn;
+    MessageSource messageSource;
 
     @GetMapping("/")
     public String home(Model model) {
-        System.out.println(hello);
-        System.out.println(evn.getProperty("my.key"));
 
-        String s = "Hello Day La Lam";
-        model.addAttribute("msg", s);
+        model.addAttribute("msg", messageSource.getMessage("my.name", null, null));
         return "index";
     }
 }
